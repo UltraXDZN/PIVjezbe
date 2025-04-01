9fdfe54ba36d402716e57bb8897af923d5ea4a77
UltraXDZN <ultraxdzn@gmail.com>
2025-04-01 23:11:52 +0200

    Vjezba03:
    Added main card outline

--- vjezbe03/app/pages/index.vue
+++ vjezbe03/app/pages/index.vue
@@ -3,5 +3,8 @@ import { ref } from 'vue'
 </script>
 
 <template>
-  Hello World
+
+  <div class="bg-[#fbfbfa] border-[#eff0f2] border-solid border-1 rounded-lg w-auto m-3 p-5 flex flex-col gap-4">
+    Hello world
+  </div>
 </template>
